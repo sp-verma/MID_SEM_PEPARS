006eed63c4d4ae16411d961ce85862b5dde069ef
Rahul <RahulKumar703@outlook.com>
2024-06-01 13:25:31 +0530

    login page removed bg

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,9 +2,7 @@ import LoginForm from "@/components/loginForm";
 
 const page = async () => {
   return (
-    <div className="flex flex-col">
-      <LoginForm />
-    </div>
+    <LoginForm />
   );
 };
 
